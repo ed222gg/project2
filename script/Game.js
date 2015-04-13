855608898d70f85ef3bf9db4e76a6e3348767d2f
ed222gg <ed222gg@student.lnu.se>
2015-04-13 07:39:07 +0000

    provar med nya knappar

--- script/Game.js
+++ script/Game.js
@@ -2,46 +2,256 @@
 
 var Game= {
 
-    pos: 0,
-    categoryNr: undefined,
-    test: undefined,
-    testStatus: undefined,
-    question: undefined,
-    choice: undefined,
-    choises: undefined,
-    chA: undefined,
-    chB: undefined,
+
+ 
+
     correct: 0,
     questionsArray: [],
+    
+     gameOn: {
+        animal: {
+            question: 'Vem väger mest?',
+            
+            answer: [ {
+                bild1: 'frg',
+                bild2: 'dfg',
+                r: ''
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                    
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                
+            }]
+        },
+    
+        geo:{
+            question: 'Vad ligger mest söder ut?',
+            
+            answer: [ {
+                bild1: '',
+                bild2: '',
+                r: ''
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                    
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                
+            }]
+        },
+             
+        music:{
+            question: 'Vilken artist/grupp bildades först?',
+            
+            answer: [ {
+                bild1: '',
+                bild2: '',
+                r: ''
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                    
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                
+            }]
+        },
+        
+         celeb:{
+            question: 'Vem är äldst?',
+            
+            answer: [ {
+                bild1: '',
+                bild2: '',
+                r: ''
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                    
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                
+            }]
+        },
+              architecture:{
+            question: 'Vem är äldst?',
+            
+            answer: [ {
+                bild1: '',
+                bild2: '',
+                r: ''
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                    
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                
+            },
+            {   bild1: '',
+                bild2: '',
+                r: ''
+                
+            }]
+        },
+     },
 
 
     init: function() { 
-        var radios = document.getElementsByName('category'); // samla alla cetegoryelementen i en lista
-        console.log(radios)
         
-        for (var i = 0; i < radios.length; i++) {   
-            eventClick(i);
-        }
-        //funktionen eventclick som vi kallar på i forloopen ovan,
-        //lägger en eventlistener på alla elementen i radios
-        function eventClick(categoryId){
-            radios[categoryId].addEventListener("click", function() {
-            if (radios[categoryId].checked)
-            {
-                
-                //nu har vi checkat samt vet vilket id categorin har. nu kan vi sätta rätt frågor. next step.
-                //hmm hur var det jag hade tänkt nu då...
-                console.log(categoryId)
-                
-            }
-           
-           
-            });
-        }
-        
-       
+      var animalBtn = document.getElementById("li0");
+      animalBtn.addEventListener("click", function() {
+          	document.getElementById("categories").innerHTML = "";
+          	document.getElementById("CategoryTitle").innerHTML = "Vem väger mest?";
+          	Game.ran('animal');
+          
+      });
+      
+      var geoBtn = document.getElementById("li1");
+       geoBtn.addEventListener("click", function() {
+           document.getElementById("categories").innerHTML = "";
+          	document.getElementById("CategoryTitle").innerHTML = "Vad ligger mest söder ut?";
+          Game.ran('geo');
+          
+      });
+      
+      var musicBtn = document.getElementById("li2");
+      musicBtn.addEventListener("click", function() {
+          document.getElementById("categories").innerHTML = "";
+          	document.getElementById("CategoryTitle").innerHTML = "Vilken artist/grupp bildades först?";
+          Game.ran('music');
+          
+      });
+      
+       var celebBtn = document.getElementById("li3");
+      celebBtn.addEventListener("click", function() {
+           document.getElementById("categories").innerHTML = "";
+          	document.getElementById("CategoryTitle").innerHTML = "vem är äldst?";
+          Game.ran('celeb');
+          
+      });
+      
+       var arcBtn = document.getElementById("li4");
+      arcBtn.addEventListener("click", function() {
+         document.getElementById("categories").innerHTML = "";
+          	document.getElementById("CategoryTitle").innerHTML = "Vilken byggnad är högst?";
+          Game.ran('architecture'); 
+          
+      });
+
+    
+
 
     },
+    
+    ran: function(catName) {
+        //Game.gameOn.catName.rubrik // rubriken
+        //Game.gameOn.catName.question.length // gör en random funktion med längden av denna
+        var random = Math.floor((Math.random() * Game.gameOn[catName].answer.length) +1); 
+        
+        
+        Game.gameOn[catName].answer[random]; // x = svaret random ger
+        console.log(random);
+        
+        
+        
+    }
+    
 };
 //När sidan har laddads in så anropas init.
 window.onload = Game.init;
+
+
+ /* // main funktionen   
+   var template = document.querySelector('#template');
+   var gameLayout = template.content.querySelector('#gameLayout').cloneNode(true);
+   
+   // om du vill hämta element i main funktionen
+   gameLayout.questionP = gameLayout.querySelector('#question');
+   gameLayout.questionP.innerHTML ='vem van vm 2011';
+   
+   // lägger ut allt 
+   container.appendChild(gameLayout);*/
